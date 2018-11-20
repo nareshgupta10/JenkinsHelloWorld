a8f741586c2cc2b01059b944bd75f63a865954ab
nareshgupta10 <nareshgupta10@gmail.com>
2018-11-21 01:39:52 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld{
 	public static void main (String args[]){
-	System.out.println("******Welcome to the Jenkins build configuration to test the build tiggered after change******");
+	System.out.println("******Welcome to the Jenkins build configuration to test the webhook pooling******");
 }
 
 }
